49b0cc4fb53a2231b0924bf52fc20d0edc59665c
Raine Adams <rainedoescode@gmail.com>
2024-01-02 15:28:46 -0600

    Additional research notes and game framework

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 
 let currentPressure = 0;
-let keypressPressure = 50;
+let keypressPressure = 500; // Generalized value for 50g keypress, 500 Pascal
+let pressureBulbs = 0;
+let pressureBulbPressure = 1866.5;
 
 const theNumberElem = document.getElementById('theNumber');
 
 const pressureButton = () => {
     currentPressure += keypressPressure;
-    theNumberElem.innerHTML = currentPressure.toString();
+    theNumberElem.innerHTML = currentPressure;
+};
+
+const squeezeBulb = () => {
+    currentPressure += pressureBulbPressure;
+    theNumberElem.innerHTML = currentPressure;
 };
